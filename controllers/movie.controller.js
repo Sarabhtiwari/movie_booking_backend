f4b96024ece16b033405ce63d8143daecb77efba
Brimhesh Tiwari <tiwaribrimhesh@gmail.com>
2026-03-02 13:47:25 +0530

    Feat: added get movie api

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -20,6 +20,26 @@ const createMovie = async (req,res) => {
     }
 };
 
+const deleteMovie = async(req,res) => {
+    try {
+        const response = await Movie.deleteOne({_id: req.params.id});
+        return res.status(200).json({
+            success: true,
+            error: {},
+            message: 'Successfully deleted the movie',
+            data: response
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            error: error,
+            message: 'Something went wrong',
+            data: {}
+        });
+    }
+};
 module.exports = {
-    createMovie
+    createMovie,
+    deleteMovie
 }
--- routes/movie.route.js
+++ routes/movie.route.js
@@ -3,6 +3,8 @@ const MovieController = require('../controllers/movie.controller')
 const routes = (app) => {
     //takes express app object as parameter
     app.post('/mba/api/v1/movies', MovieController.createMovie);
+
+    app.delete('/mba/api/v1/movies/:id',MovieController.deleteMovie)
 }
 
 module.exports = routes;
