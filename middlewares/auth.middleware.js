68f9744fb08e7a3020f023abe5798604bc186b9b
Brimhesh Tiwari <tiwaribrimhesh@gmail.com>
2026-03-06 13:04:25 +0530

    Feat:  added vlaidator for signin

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 const { errorResponseBody } = require("../utils/responsebody")
 
+//for signup 
 const validateAuthRequest = async (req,res,next) => {
     if(!req.body.name){
         errorResponseBody.err = "Name of the user not present in the request"
@@ -16,6 +17,21 @@ const validateAuthRequest = async (req,res,next) => {
     }
     next();
 }
+
+//for signin
+const validateSignInRequest = async (req,res,next) => {
+    if(!req.body.email){
+        errorResponseBody.err = "Email of the user not present in the signn in request"
+        return res.status(400).json(errorResponseBody);
+    }
+    if(!req.body.password){
+        errorResponseBody.err = "password of the user not present in the sign in request"
+        return res.status(400).json(errorResponseBody);
+    }
+    next();
+}
+
 module.exports = {
-    validateAuthRequest
+    validateAuthRequest,
+    validateSignInRequest
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -6,7 +6,7 @@ const routes = (app) => {
     app.post('/mba/api/v1/auth/signup',
     authMiddleware.validateAuthRequest,authController.signup);
 
-    app.post('/mba/api/v1/auth/signin',authController.signin);
+    app.post('/mba/api/v1/auth/signin',authMiddleware.validateSignInRequest,authController.signin);
 }
 
 module.exports = routes;
